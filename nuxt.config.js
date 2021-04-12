73a2c0c3a81d50ead6ab2d01c83adb1a70f9cbc5
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-04-12 21:49:25 +0700

    finishing

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
+  plugins: [{src:"~plugins/vue-cool-lightbox.js", mode:"client"}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -34,6 +34,7 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,84 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        info-gempa-terkini
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="bg-gray-300 w-screen min-h-screen">
+    <div class="w-full" v-if="info != null" >
+      <div class="flex justify-center">
+        <div>
+          <h1 class="font-bold text-2xl text-center mt-4">Info Gempa Terkini</h1> 
+          <div class="my-5">
+            <p><span class="font-bold">Tanggal : </span>{{info.Infogempa.gempa.Tanggal._text}}</p>
+            <p><span class="font-bold">Jam : </span>{{info.Infogempa.gempa.Jam._text}}</p>
+            <p><span class="font-bold">Magnitude : </span>{{info.Infogempa.gempa.Magnitude._text}}</p>
+            <p><span class="font-bold">Kedalaman : </span>{{info.Infogempa.gempa.Kedalaman._text}}</p>
+            <p><span class="font-bold">Kordinat : </span>{{info.Infogempa.gempa.point.coordinates._text}}</p>
+            <p><span class="font-bold">Lintang : </span>{{info.Infogempa.gempa.Lintang._text}}</p>
+            <p><span class="font-bold">Bujur : </span>{{info.Infogempa.gempa.Bujur._text}}</p>
+            <p><span class="font-bold">Wilayah : </span>{{info.Infogempa.gempa.Wilayah._text}}</p>
+            <p><span class="font-bold">Potensi : </span>{{info.Infogempa.gempa.Potensi._text}}</p>
+            <p><span class="font-bold">Dirasakan : </span>{{info.Infogempa.gempa.Dirasakan._text}}</p>
+          </div>
+          <!-- <img :src="`https://data.bmkg.go.id/DataMKG/TEWS/${info.Infogempa.gempa.Shakemap._text}`" alt="" srcset=""> -->
+          <CoolLightBox 
+            :items="items" 
+            :index="index"
+            @close="index = null">
+          </CoolLightBox>
+
+          <div class="images-wrapper">
+            <div
+              class="image"
+              v-for="(image, imageIndex) in items"
+              :key="imageIndex"
+              @click="index = imageIndex"
+              :style="{ backgroundImage: 'url(' + image + ')' }"
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+import xmljs from 'xml-js'
+export default {
+  name: "index",
+  data(){
+    return{
+      info:null,
+      items:[],
+      // items: [
+      //   'https://www.gotravelly.com/blog/wp-content/uploads/2019/10/Gunung-Fuji-Jepang-1024x640.jpg',
+      //   'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1557340276.jpg?crop=0.666xw:1.00xh;0.168xw,0&resize=640:*',
+      // ],
+      index: null
+    }
+  },
+  async mounted(){
+      var data = await (await axios.get("https://data.bmkg.go.id/DataMKG/TEWS/autogempa.xml")).data
+      this.info = JSON.parse(xmljs.xml2json(data,{compact:true,spaces:2}))
+      this.items.push(`https://data.bmkg.go.id/DataMKG/TEWS/${this.info.Infogempa.gempa.Shakemap._text}`)
+  }
+}
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style scoped>
+.images-wrapper {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  justify-content:center;  
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.images-wrapper .image {
+  min-width: 600px;
+  min-height: 720px;
+  background-size: auto;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.images-wrapper-divs {
+  margin-bottom: 40px;
 }
 
-.links {
-  padding-top: 15px;
+.images-wrapper-img img {
+  object-fit: cover;
 }
 </style>
